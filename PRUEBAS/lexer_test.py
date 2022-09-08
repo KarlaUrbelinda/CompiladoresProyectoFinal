51c431067db1b51ac70de2c9f8d027aa700f957c
KarlaUYuca <87000494+KarlaUrbelinda@users.noreply.github.com>
2022-09-08 17:01:18 -0500

    main

--- PRUEBAS/lexer_test.py
+++ PRUEBAS/lexer_test.py
@@ -1,12 +1,8 @@
 from unittest import TestCase
+#Importaciones
 from typing import List
-
-from PLHL.token import (
-    Token,
-    TokenType,
-)
+from PLHL.token import(Token,TokenType,)
 from PLHL.lexer import Lexer
-
 class LexerTest(TestCase):
 
     def test_illegal(self) -> None:
@@ -23,19 +19,4 @@ class LexerTest(TestCase):
             Token(TokenType.ILLEGAL, '@'),
         ]
 
-        self.assertEqual(tokens, expected_tokens)
-
-    def test_one_character_operator(self) -> None:
-        source: str = '=+'
-        lexer: Lexer = Lexer(source)
-
-        tokens: List[Token] = []
-        for i in range(len(source)):
-            tokens.append(lexer.next_token())
-
-        expected_tokens: List[Token] = [
-            Token(TokenType.ASSIGN, '='),
-            Token(TokenType.PLUS, '+'),
-        ]
-
-        self.assertEqual(tokens, expected_tokens)
+        self.assertEquals(tokens, expected_tokens)
